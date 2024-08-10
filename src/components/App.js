6ad23485b3e9a6d9e105dcf5b63887eaa136332b
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-08-11 00:48:29 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,61 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
+  const [visible, setVisible] = useState({ hover1: false, hover2: false });
+  const hoverinFunction = (e) => {
+    // e.stopPropogation();
+
+ if(e.target.id=="hover1"){
+  console.log("hover1");
+  setVisible((prev)=>{
+
+    return {...prev ,["hover1"]:true};
+  })
+ }
+ else{
+  console.log("hover2");
+
+  setVisible((prev)=>{
+    return {...prev ,["hover2"]:true};
+  })
+ }
+  }
+
+  const hoveroutFunction = (e) => {
+    // e.stopPropogation();
+
+    if(e.target.id=="hover1"){
+      
+     setVisible((prev)=>{
+       return {...prev ,["hover1"]:false};
+     })
+    }
+    else{
+     setVisible((prev)=>{
+       return {...prev ,["hover2"]:false};
+     })
+    }
+  }
+  console.log(visible);
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      {<h2 id="hover1" onMouseEnter={hoverinFunction}
+        onMouseLeave={hoveroutFunction}>
+       { visible.hover1 ? <Tooltip text={"This is a tooltip"} />: ""}
+        Hover over me
+      </h2>}
+
+      <p id="hover2" onMouseEnter={hoverinFunction}
+        onMouseLeave={hoveroutFunction}
+      >Hover over me to see another tooltip
+
+        {visible.hover2 ? <Tooltip text={"This is a tooltip"} />: ""}
+      </p>
+
     </div>
   )
 }
---
+++ src/components/Tooltip.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Tooltip = ({text}) => {
+
+
+    
+
+    let style={
+        padding:"15px",
+        backgroundColor:"red",
+        color:"white",
+        width:"200px",
+        height:"100px",
+        borderRadius:"30px",
+        display:"flex",
+        alignItem:"center",
+        justifyContent:"center"
+
+    }
+  return (
+    
+    <div
+     className='tooltip'
+     >
+     
+        <div style={style}>{text}</div>
+    </div>
+        
+  )
+}
+
+export default Tooltip
