5a34e8a4a7a89546c94c7e05ef36fdcabcdf165b
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-08-15 14:41:10 +0530

    saved

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,30 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Tooltip = ({text}) => {
+const Tooltip = ({text,children}) => {
+  const [showTooltip, setShowTooltip]=useState(false);
+ return(
+  <div
+   onMouseEnter={()=>setShowTooltip(true)}
+   onMouseLeave={()=>setShowTooltip(false)}
+  >
+    {children}
+    {showTooltip && <div className='tooltiptext' style={{
+      width:"300px",
+      backgroundColor:"red",
+      color:"white",
+      height:"100px",
+      borderRadius:"20px",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"center"
+    }}>{text}</div>}
 
-
-    
-
-    let style={
-        padding:"15px",
-        backgroundColor:"red",
-        color:"white",
-        width:"200px",
-        height:"100px",
-        borderRadius:"30px",
-        display:"flex",
-        alignItem:"center",
-        justifyContent:"center"
-
-    }
-  return (
-    
-    <div>
-     
-        <p className='tooltip' style={style}>{text}</p>
-    </div>
-        
-  )
+  </div>
+ )
 }
 
 export default Tooltip
